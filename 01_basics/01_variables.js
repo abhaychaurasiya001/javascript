30539b305aa687fb4a7594fcb7ee1cb6da5b51e8
ABHAY CHAURASIYA <chaurasiyaabhay664@gmail.com>
2026-07-03 12:11:37 +0530

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456789;
+let accountEmail = "cahurasiyaabhay464@gmail.com";
+var accountPassword = "Abhay@123";
+accountCity = "Pune";
+
+
+// accountId =232;
+
+accountEmail = "anahand@gmail.com";
+accountPassword = "Anahand@123";
+accountCity = "Mumbai";
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
