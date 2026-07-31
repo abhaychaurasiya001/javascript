e07f6c847aea5ccf2e8a030fd840d4b27f8c536f
abhay chaurasiya <chaurasiyaabhay664>
2026-07-31 22:35:06 +0530

    about datatype and memory allocation with little bit about string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "abhay"
+const repocount = 50
+
+// console.log(name+repocount+"value")
+
+console.log(`my name is ${name} and my repo count is ${repocount} and so on`)
+
+const gamename =new String('Call of Duty');
