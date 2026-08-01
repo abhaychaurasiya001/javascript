d982254e5d3e547f0ad51c1175b7ad38dda816ab
abhay chaurasiya <chaurasiyaabhay664>
2026-08-01 20:57:47 +0530

    number and math

---
+++ 01_basics/06_numandmaths.js
@@ -0,0 +1,37 @@
+const score =400
+// console.log(score)
+
+const balence = new Number(1000)
+// console.log(balence)
+
+// console.log(balence.toString().length)
+// console.log(balence.toFixed(1));
+
+
+const othernumber = 232.253165;
+
+// console.log(othernumber.toPrecision(4) )
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ==================maths============
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(5.6));
+// console.log(Math.min(5,6,7,8,9,10));
+// console.log(Math.max(5,6,7,8,9,10));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)))+min
