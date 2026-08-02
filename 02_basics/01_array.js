ee9d618168a099f883fce2f9d883b58ca490ca74
abhay chaurasiya <chaurasiyaabhay664>
2026-08-02 09:57:58 +0530

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+// ++++++++++++++++++ARRAY+++++++++++++++++
+
+const myArr = [1,2,3,4,5,6,7]
+const myHeros=["soiderman","ironman"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0])
+
+//methods
+
+// console.log(myArr);
+// myArr.push(8)
+// myArr2.push(5)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(newArr)
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+
+
+console.log("B",myArr);
+const myn2 = myArr.slice(1,3);
+console.log(myn2);
+
+
+console.log("c",myArr);
+const myn3 = myArr.splice(1,3);
+console.log(myn3);
+
