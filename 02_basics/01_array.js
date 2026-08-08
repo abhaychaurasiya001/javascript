f093eaf052cc2a64e5503e123b1a57f167da2650
abhay chaurasiya <chaurasiyaabhay664>
2026-08-08 18:17:21 +0530

    JSON formate

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -8,24 +8,24 @@ console.log(myArr[0])
 
 //methods
 
-// console.log(myArr);
-// myArr.push(8)
-// myArr2.push(5)
-// myArr.pop()
+console.log(myArr);
+myArr.push(8)
+myArr2.push(5)
+myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
-
-
-
-// console.log(myArr.includes(5));
-// console.log(myArr.indexOf(5));
-
-// const newArr = myArr.join()
+myArr.unshift(9)
+myArr.shift()
 
 
-// console.log(myArr);
-// console.log(newArr)
+
+console.log(myArr.includes(5));
+console.log(myArr.indexOf(5));
+
+const newArr = myArr.join()
+
+
+console.log(myArr);
+console.log(newArr)
 
 console.log("A",myArr);
 const myn1 = myArr.slice(1,3);
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,7 +29,7 @@ const obj3 = {5: "e", 6: "f"}
 
 const obj8 = {...obj1, ...obj2, ...obj3}
 
-console.log(obj8);
+// console.log(obj8);
 
 const users = [
     {
@@ -51,7 +51,7 @@ const users = [
 
 ]
 users[1].name
-// console.log(users[1].name);
+console.log(users[1].name);
 console.log(tinderuser);
 
 console.log(Object.keys(tinderuser));
@@ -62,3 +62,91 @@ console.log(tinderuser.hasOwnProperty("naabhame"));
 console.log(tinderuser.isPrototypeOf("name"));
 console.log(tinderuser.valueOf("id"));
 console.log(tinderuser.toString ("id"));
+
+
+const course = {
+    coursename: "JavaScript",
+    price: 299,
+    courseInstructor: "Simran",
+}
+
+const {courseInstructor:instructor} = course
+console.log(instructor);
+
+// ===================JSON fole formate =======================
+// {
+//     // this is used for object type jso file formate
+// }
+// [
+//     []: used for array type json file formateit contains multiple objects in this form 
+//     [{},
+//         {},
+//         {},
+//     ]
+// ]
+// {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "Miss",
+//         "first": "Jennie",
+//         "last": "Nichols"
+//       },
+//       "location": {
+//         "street": {
+//           "number": 8929,
+//           "name": "Valwood Pkwy",
+//         },
+//         "city": "Billings",
+//         "state": "Michigan",
+//         "country": "United States",
+//         "postcode": "63104",
+//         "coordinates": {
+//           "latitude": "-69.8246",
+//           "longitude": "134.8719"
+//         },
+//         "timezone": {
+//           "offset": "+9:30",
+//           "description": "Adelaide, Darwin"
+//         }
+//       },
+//       "email": "jennie.nichols@example.com",
+//       "login": {
+//         "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+//         "username": "yellowpeacock117",
+//         "password": "addison",
+//         "salt": "sld1yGtd",
+//         "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+//         "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+//         "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+//       },
+//       "dob": {
+//         "date": "1992-03-08T15:13:16.688Z",
+//         "age": 30
+//       },
+//       "registered": {
+//         "date": "2007-07-09T05:51:59.390Z",
+//         "age": 14
+//       },
+//       "phone": "(272) 790-0888",
+//       "cell": "(489) 330-2385",
+//       "id": {
+//         "name": "SSN",
+//         "value": "405-88-3636"
+//       },
+//       "picture": {
+//         "large": "https://randomuser.me/api/portraits/men/75.jpg",
+//         "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+//         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+//       },
+//       "nat": "US"
+//     }
+//   ],
+//   "info": {
+//     "seed": "56d27f4a53bd5441",
+//     "results": 1,
+//     "page": 1,
+//     "version": "1.4"
+//   }
+// }
