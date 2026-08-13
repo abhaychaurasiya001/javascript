02ce4d7a305e53b45b383985eaa0f9696e888001
abhay chaurasiya <chaurasiyaabhay664>
2026-08-14 00:49:56 +0530

    if and else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,47 @@
+// if
+
+if(true){
+    // if condition is true then code will execute
+}
+if(false){
+
+    // code will not execute
+}
+
+// const isUserloggedin = true
+// const temprtature = 41
+// if(temprtature!=41){
+//     console.log("yes , temprature is less than 50");
+    
+
+// }
+// else{
+//     console.log("Temprature is greater than 50");
+// }
+
+
+
+// const Score = 200
+
+// if (Score>100){
+//     const power = "fly"
+//     console.log(`user power:${power}`);
+    
+// }
+// console.log(`user power:${power}`);
+
+// if we use const in  code then it will dxecuted btt if we usess var on the place of CONST and put the whole line OUTSIDE THE IF STATEMEMT .... both will run .....................
+
+
+
+const balance = 1000
+
+// if (balance>500) console.log("test"),console.log("test2")
+
+if (balance>500){
+    console.log("less than 500");
+   
+
+}else if(balance<750){
+    console.log("less than 750")
+}
